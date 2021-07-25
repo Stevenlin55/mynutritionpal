212429878a6145c2bec1a5f34ff6b65b35538a01
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2021-07-25 17:34:45 -0500

    Created backend

---
+++ backend/models/food.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const foodSchema = new Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  calories: { type: Number, required: true },
+  protein: { type: Number, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamps: true,
+});
+
+const food = mongoose.model('food', foodSchema);
+
+module.exports = food;
---
+++ backend/routes/foods.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+let Food = require('../models/food.model');
+
+router.route('/').get((req, res) => {
+  Food.find()
+    .then(foods => res.json(foods))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const username = req.body.username;
+  const name = req.body.name;
+  const calories = Number(req.body.calories);
+  const protein = Number(req.body.protein);
+  const date = Date.parse(req.body.date);
+
+  const newFood = new Food({
+    username,
+    name,
+    calories,
+    protein,
+    date,
+  });
+
+  newFood.save()
+  .then(() => res.json('Food added!'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Food.findById(req.params.id)
+    .then(food => res.json(food))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Food.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Food deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Food.findById(req.params.id)
+    .then(food => {
+      food.name = req.body.name;
+      food.calories = Number(req.body.calories);
+      food.protein = Number(req.body.protein);
+      food.save()
+        .then(() => res.json('Food updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
