db20aa0c48fc64abb72dab4377d8d06311a7db9f
Stevenlin55 <70299036+Stevenlin55@users.noreply.github.com>
2021-08-10 19:50:59 -0500

    Created the UI component for date selector

--- src/components/FoodDiary.js
+++ src/components/FoodDiary.js
@@ -1,56 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { FaCaretSquareLeft, FaCaretSquareRight } from "react-icons/fa";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-
-const Food = props => (
-    <tr>
-      <td>{props.food.username}</td>
-      <td>{props.food.name}</td>
-      <td>{props.food.calories}</td>
-      <td>{props.food.protein}</td>
-      <td>
-        <Link to={"/edit/"+props.food._id}>edit</Link> | <a href="#" onClick={() => { props.deleteFood(props.food._id) }}>delete</a>
-      </td>
-    </tr>
-  )
+const Food = (props) => (
+  <tr>
+    <td>{props.food.username}</td>
+    <td>{props.food.name}</td>
+    <td>{props.food.calories}</td>
+    <td>{props.food.protein}</td>
+    <td>
+      <Link to={"/edit/" + props.food._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteFood(props.food._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
 
 const FoodDiary = () => {
-    const [foods, setFoods] = useState([]);
+  const [foods, setFoods] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/foods/')
-          .then(res => {
-            setFoods(res.data)
-          }).catch(error => console.log(error));
-      },[]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/foods/")
+      .then((res) => {
+        setFoods(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    function deleteFood(id) {
-        axios.delete('http://localhost:5000/foods/'+id)
-            .then(res => {
-                console.log(res.data, " is deleted");
-            })
-            .catch(error => console.log(error));
-        
-        setFoods(foods.filter(food => food._id !== id));
-    }
+  function deleteFood(id) {
+    axios
+      .delete("http://localhost:5000/foods/" + id)
+      .then((res) => {
+        console.log(res.data, " is deleted");
+      })
+      .catch((error) => console.log(error));
 
-    function foodList() {
-        return foods.map(currentFood => {
-          return <Food food={currentFood} deleteFood={deleteFood} key={currentFood._id}/>;
-        })
-    }
+    setFoods(foods.filter((food) => food._id !== id));
+  }
 
-    return (
-        <div className="container bg-gray-50 my-8">
-          <div className="flex">
-            <p>Your Food Diary For: </p>
-            <div className="flex">
-              <div className="bg-blue-700 w-7 h-7"></div>
-            </div>
+  function foodList() {
+    return foods.map((currentFood) => {
+      return (
+        <Food
+          food={currentFood}
+          deleteFood={deleteFood}
+          key={currentFood._id}
+        />
+      );
+    });
+  }
+
+  return (
+    <div className="container my-8">
+      <div className="flex items-center justify-center">
+        <div>Your Food Diary For:</div>
+        <div className="flex">
+          <FaCaretSquareLeft size={30} color={"blue"} className="-mr-px" />
+          <div className="flex items-center justify-center bg-blue-600 rounded-sm h-6 mt-0.5 text-white px-4 py-1">
+            Tuesday, July 21, 2021
           </div>
+          <FaCaretSquareRight size={30} color={"blue"} className="-ml-px" />
+        </div>
+      </div>
 
-{/* 
+      <div className="mx-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Calories (kcal)</th>
+              <th>Protein (g)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Grilled Chicken Sandwich</td>
+              <td>400</td>
+              <td>40</td>
+            </tr>
+            <tr class="bg-emerald-200">
+              <td>
+                Egg Tart
+              </td>
+              <td>200</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      {/* 
         <div className="hidden">
           <h3>Logged Foods</h3>
           <table className="table">
@@ -67,9 +116,8 @@ const FoodDiary = () => {
             </tbody>
           </table>
         </div> */}
-       
-        </div>
-    );
+    </div>
+  );
 };
 
 export default FoodDiary;
